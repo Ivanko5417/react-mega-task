d99e43ba86d54c9a36d992f8134fbff45d191bbf
Ivanko5417 <v.drobysh2011@gmail.com>
2022-02-13 21:24:00 +0300

    fix tests

--- e2e.test.js
+++ e2e.test.js
@@ -1,21 +1,25 @@
-const puppeteer = require("puppeteer");
+import React from 'react';
+import { shallow, mount, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Foo from './src/Foo';
 
-describe("App.js", () => {
-  let browser;
-  let page;
+configure({ adapter: new Adapter() });
 
-  beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
+
+describe('A suite', function() {
+  it('should render without throwing an error', function() {
+    expect(shallow(<Foo />).contains(<div className="foo">Bar</div>)).toBe(true);
   });
 
-  it("contains the welcome text", async () => {
-    await page.goto("https://www.google.com/");
-    await page.waitForSelector(".uU7dJb");
-    const text = await page.$eval(".uU7dJb", (e) => e.textContent);
-    console.log(text);
-    expect(text).toContain('Беларусь');
+  it('should be selectable by class "foo"', function() {
+    expect(shallow(<Foo />).is('.foo')).toBe(true);
   });
 
-  afterAll(() => browser.close());
+  it('should mount in a full DOM', function() {
+    expect(mount(<Foo />).find('.foo').length).toBe(1);
+  });
+
+  it('should render to static HTML', function() {
+    expect(render(<Foo />).text()).toEqual('Bar');
+  });
 });
